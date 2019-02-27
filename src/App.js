5f32160d888f3ce266052b587c42795d9dfc1d2f
Oscar Alba <oscaralba33@gmail.com>
2019-02-26 19:17:47 -0500

    assitance

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <Login/>
+                    <Assistance/>
                 </header>
             </div>
         );
---
+++ src/Meeting/Assistance.js
@@ -0,0 +1,24 @@
+import React from "react";
+import TextField from '@material-ui/core/TextField';
+import Paper from "@material-ui/core/es/Paper/Paper";
+import './Assistance.css';
+import logo from './../img/Logo.png';
+import user from './../img/user.svg';
+import password from './../img/password.svg';
+import {*} from "@material-ui/core";
+import Link from "@material-ui/core/Link";
+
+
+export class Assistance extends React.Component {
+
+  render(){
+    return(
+
+      <div>
+        <h1>TODO ESTA BIEN</h1>
+      </div>
+
+
+    );
+  }
+}
