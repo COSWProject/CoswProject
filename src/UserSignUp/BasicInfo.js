62f09ca270c91ee0d8abf8cbf9c6c093aed3b3b4
DianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2019-02-26 21:45:41 -0500

    user Sign up functionality withput icons

---
+++ src/UserSignUp/BasicInfo.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+
+function AddressForm() {
+    return (
+        <React.Fragment>
+            <Typography variant="h6" gutterBottom>
+                Basic Information
+            </Typography>
+            <Grid container spacing={24}>
+                <Grid item xs={12} sm={6}>
+                    <Grid item>
+                        <business />
+                    </Grid>
+                    <TextField
+                        required
+                        id="email"
+                        name="email"
+                        label="Email"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id="password"
+                        name="password"
+                        label="Password"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id="mobile"
+                        name="mobile"
+                        label="Mobile Phone"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        id="homephone"
+                        name="homephone"
+                        label="Home Phone"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        id="address1"
+                        name="address1"
+                        label="Address line 1"
+                        fullWidth
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id="city"
+                        name="city"
+                        label="City"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField id="state" name="state" label="State/Province/Region" fullWidth />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id="zip"
+                        name="zip"
+                        label="Zip / Postal code"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id="country"
+                        name="country"
+                        label="Country"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12} >
+                    <TextField
+                        id="occupation"
+                        name="occupation"
+                        label="Occupation"
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item xs={12} >
+                    <TextField
+                        id="organization"
+                        name="organization"
+                        label="Organization"
+                        fullWidth
+                        autoComplete="billing country"
+                    />
+                </Grid>
+
+            </Grid>
+        </React.Fragment>
+    );
+}
+
+export default AddressForm;
