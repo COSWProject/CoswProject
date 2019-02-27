93ee039b82fb51756913101ff02df4685239a0bd
Camilo Lopez <camilino123@gmail.com>
2019-02-27 07:49:33 -0500

    Route added, EnterpriseSignUp component started

---
+++ src/SignUp/EnterpriseSignUp.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class EnterpriseSignUp extends Component {
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+export default EnterpriseSignUp;
