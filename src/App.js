c943df6e23602087e41de1f9f6acc20c5b186b3c
Camilo Lopez <camilino123@gmail.com>
2019-02-27 08:04:22 -0500

    Handles created, inputsText props added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
 import {Route, Link, BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
+import EnterpriseSignUp from "./SignUp/EnterpriseSignUp";
 
 class App extends Component {
     render() {
+
+        const enterpriseView = () => (
+            <EnterpriseSignUp/>
+        );
+
         return (
             <div className="App">
                 <header className="App-header">
                     <Router>
-                        <Route path="enterpriseregister"/>
+                        <Route path="/enterpriseregister" component={enterpriseView}/>
                     </Router>
                 </header>
             </div>
--- src/SignUp/EnterpriseSignUp.js
+++ src/SignUp/EnterpriseSignUp.js
@@ -1,7 +1,88 @@
 import React, {Component} from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 class EnterpriseSignUp extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            ubication: "",
+            email: "",
+            phone: 0,
+            nit: 0,
+            password: ""
+        }
+
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeUbication = this.handleChangeUbication.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangePhone = this.handleChangePhone.bind(this);
+        this.handleChangeNit = this.handleChangeNit.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+    }
+
+    handleChangeName(e) {
+        this.setState({name: e.target.value});
+    }
+
+    handleChangeUbication(e) {
+        this.setState({ubication: e.target.value});
+    }
+
+    handleChangeEmail(e) {
+        this.setState({email: e.target.value});
+    }
+
+    handleChangePhone(e) {
+        this.setState({phone: e.target.value});
+    }
+
+    handleChangeNit(e) {
+        this.setState({nit: e.target.value});
+    }
+
+    handleChangePassword(e) {
+        this.setState({password: e.target.value});
+    }
+
     render() {
+
+        const inputs = [
+            {
+                label: "Name",
+                type: "text",
+                onchange: this.handleChangeName
+            }, {
+                label: "Ubication",
+                type: "text",
+                onchange: this.handleChangeUbication
+            }, {
+                label: "Email",
+                type: "text",
+                onchange: this.handleChangeEmail
+            }, {
+                label: "Phone",
+                type: "number",
+                onchange: this.handleChangePhone
+            }, {
+                label: "Nit",
+                type: "number",
+                onchange: this.handleChangeNit
+            }, {
+                label: "Password",
+                type: "password",
+                onchange: this.handleChangePassword
+            }
+        ];
+
+
+        const inputTexts = inputs.map((x) => {
+
+        })
+
         return (
             <>
             </>
