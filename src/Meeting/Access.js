1990a91c422637d7ba89a374ec5ff9212f88f516
Oscar Alba <oscaralba33@gmail.com>
2019-02-26 23:16:42 -0500

    Access Component finished

--- src/Meeting/Access.js
+++ src/Meeting/Access.js
@@ -11,11 +11,13 @@ export class Access extends React.Component {
 
   render(){
     return (
-      <div className="content">
-        <div>
+      <div className="content_access">
+        <div className="pass">
           <h2>QR PASS</h2>
           <p>Present this at the reception, to pass over.</p>
           <img src={QR}/>
+          <p>Escuela Colombiana de Ingenieros Julio Garavito</p>
+          <h2>Important things you need to remember</h2>
           <p>Hour: 8:30</p>
           <p>Address: AK 45 (Autonorte) #205-59</p>
           <p>Invited: Oswaldo Castillo</p>
