746b22ebb89d0fa92cda1b7531d69b948d12dc53
danielagon <daniela.gonzalezramirez@hotmail.com>
2019-02-27 00:08:40 -0500

    User profile view

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Login} from "./Login/Login";
+import {Route, BrowserRouter as Router} from 'react-router-dom';
+import UserProfile from "./userProfile/UserProfile";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        localStorage.setItem('email','escuela@escuelaing.edu.co');
+        localStorage.setItem('password','cosw2019');
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <Router>
+                        <div>
+                            <Route exact path="/" component={Login}/>
+                            <Route path="/UserProfile" component={UserProfile}/>
+                        </div>
+                    </Router>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
