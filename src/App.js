a192bc2cd5ddc3d1e3dabe142baf6170b17c3385
Camilo Lopez <camilino123@gmail.com>
2019-02-27 10:45:26 -0500

    Create meeting done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Route, BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
+import NewMeeting from "./Enterprise/NewMeeting";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+
+        const newMeetingView = () => (
+            <NewMeeting/>
+        );
+
+        return (
+            <div className="App">
+                <Router>
+                    <Route path="/enterprise/newMeeting" component={newMeetingView}/>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
