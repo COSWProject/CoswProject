05d6f4c658b7bc9cebde0e1eebee00a8b328e4e3
Camilo Lopez <camilino123@gmail.com>
2019-02-28 23:44:39 -0500

    Drawer added to profile

--- src/Company/Main.js
+++ src/Company/Main.js
@@ -7,60 +7,16 @@ import CompanyLogin from "./Login";
 import CompanySignup from "./SignUp";
 import IconButton from "@material-ui/core/IconButton";
 import ArrowBackIos from '@material-ui/icons/ArrowBackIos';
-import classNames from "classnames";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Drawer from "@material-ui/core/Drawer";
-import Divider from "@material-ui/core/Divider";
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import MenuIcon from '@material-ui/icons/Menu';
 
-const drawerWidth = 240;
 
 const styles = theme => ({
-    root: {
-        display: 'flex',
-    }, appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    }, appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    }, menuButton: {
-        marginLeft: 12,
-        marginRight: 20,
-    }, hide: {
-        display: 'none',
-    }, drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    }, drawerPaper: {
-        width: drawerWidth,
-    }, drawerHeader: {
-        display: 'flex',
-        alignItems: 'center',
-        padding: '0 8px',
-        ...theme.mixins.toolbar,
-        justifyContent: 'flex-end',
-    }, content: {
+    grow: {
         flexGrow: 1,
-        padding: theme.spacing.unit * 3,
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginLeft: -drawerWidth,
-    }, contentShift: {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
+        textAlign: "left"
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
     },
 });
 
@@ -69,78 +25,30 @@ class Main extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            open: false,
-        };
     }
 
     handleBackButton(e) {
         console.log("Back button clicked");
     }
 
-    handleDrawerOpen = () => {
-        this.setState({open: true});
-    };
-
-    handleDrawerClose = () => {
-        this.setState({open: false});
-    };
-
     render() {
-
-        const {open} = this.state;
 
         const {classes} = this.props;
 
         return (
             <>
-                <div className={classes.root}>
-                    <CssBaseline/>
-                    <AppBar
-                        position="fixed"
-                        className={classNames(classes.appBar, {
-                            [classes.appBarShift]: open,
-                        })}
-                    >
-                        <Toolbar disableGutters={!open}>
-                            <IconButton
-                                color="inherit"
-                                aria-label="Open drawer"
-                                onClick={this.handleDrawerOpen}
-                                className={classNames(classes.menuButton, open && classes.hide)}
-                            >
-                                <MenuIcon/>
-                            </IconButton>
-                            <Typography variant="h6" color="inherit" noWrap>
-                                Persistent drawer
-                            </Typography>
-                        </Toolbar>
-                    </AppBar>
-                    <Drawer
-                        className={classes.drawer}
-                        variant="persistent"
-                        anchor="left"
-                        open={open}
-                        classes={{
-                            paper: classes.drawerPaper,
-                        }}
-                    >
-                        <div className={classes.drawerHeader}>
-                            <IconButton onClick={this.handleDrawerClose}>
-                                <ChevronLeftIcon/>
-                            </IconButton>
-                        </div>
-                        <Divider/>
-                    </Drawer>
-                    <main
-                        className={classNames(classes.content, {
-                            [classes.contentShift]: open,
-                        })}
-                    >
-                        <div className={classes.drawerHeader}/>
-
-                    </main>
-                </div>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                            <ArrowBackIos/>
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" className={classes.grow}>
+                            News
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <CompanyLogin/>
+                <CompanySignup/>
             </>
         );
     }
--- src/Company/Profile.js
+++ src/Company/Profile.js
@@ -1,35 +1,143 @@
 import React, {Component} from 'react';
 import withStyles from "@material-ui/core/styles/withStyles";
 import AppBar from "@material-ui/core/AppBar";
-import ArrowBackIos from '@material-ui/icons/ArrowBackIos';
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import classNames from "classnames";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Divider from "@material-ui/core/Divider";
 
-const styles = theme => ({});
+const drawerWidth = 240;
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    }, appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    }, appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }, menuButton: {
+        marginLeft: 12,
+        marginRight: 20,
+    }, hide: {
+        display: 'none',
+    }, drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    }, drawerPaper: {
+        width: drawerWidth,
+    }, drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    }, content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    }, contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
+});
 
 class Profile extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            open: false,
+        };
+    }
+
+    handleBackButton(e) {
+        console.log("Back button clicked");
+    }
+
+    handleDrawerOpen = () => {
+        this.setState({open: true});
+    };
+
+    handleDrawerClose = () => {
+        this.setState({open: false});
+    };
+
     render() {
 
         const {classes} = this.props;
 
+        const {open} = this.state;
+
         return (
             <>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton
-                            className={classes.menuButton}
-                            color="inherit"
-                            aria-label="Menu"
-                            onClick={this.handleBackButton}
-                        >
-                            <ArrowBackIos/>
-                        </IconButton>
-                        <Typography variant="h6" color="inherit">
-                            Easy Access
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
+                <div className={classes.root}>
+                    <CssBaseline/>
+                    <AppBar
+                        position="fixed"
+                        className={classNames(classes.appBar, {
+                            [classes.appBarShift]: open,
+                        })}
+                    >
+                        <Toolbar disableGutters={!open}>
+                            <IconButton
+                                color="inherit"
+                                aria-label="Open drawer"
+                                onClick={this.handleDrawerOpen}
+                                className={classNames(classes.menuButton, open && classes.hide)}
+                            >
+                                <MenuIcon/>
+                            </IconButton>
+                            <Typography variant="h6" color="inherit" noWrap>
+                                Persistent drawer
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                    <Drawer
+                        className={classes.drawer}
+                        variant="persistent"
+                        anchor="left"
+                        open={open}
+                        classes={{
+                            paper: classes.drawerPaper,
+                        }}
+                    >
+                        <div className={classes.drawerHeader}>
+                            <IconButton onClick={this.handleDrawerClose}>
+                                <ChevronLeftIcon/>
+                            </IconButton>
+                        </div>
+                        <Divider/>
+                    </Drawer>
+                    <main
+                        className={classNames(classes.content, {
+                            [classes.contentShift]: open,
+                        })}
+                    >
+                        <div className={classes.drawerHeader}/>
+
+                    </main>
+                </div>
             </>
         );
     }
