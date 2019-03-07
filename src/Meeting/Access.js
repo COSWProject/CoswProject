75e47e9007efaa8c810e56f2a2aea5c912c77303
Oscar Alba <oscaralba33@gmail.com>
2019-03-06 22:34:39 -0500

    Header Added in meetings

--- src/Meeting/Access.js
+++ src/Meeting/Access.js
@@ -2,6 +2,14 @@ import React from "react";
 import './Access.css';
 import QR from './../img/QR.png';
 import Link from "@material-ui/core/Link";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import KeyboardArrowLeft from "@material-ui/core/es/internal/svg-icons/KeyboardArrowLeft";
+import Typography from "@material-ui/core/Typography";
+
+
 
 export class Access extends React.Component {
 
@@ -10,20 +18,35 @@ export class Access extends React.Component {
   }
 
   render(){
+
+    const {classes} = this.props;
+
     return (
+      <>
+      <CssBaseline/>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className="menuButton" color="inherit" aria-label="Menu">
+            <KeyboardArrowLeft/>
+          </IconButton>
+          <Typography variant="h6" color="inherit" className="grow">
+            QR PASS
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <div className="content_access">
         <div className="pass">
-          <h2>QR PASS</h2>
-          <p>Present this at the reception, to pass over.</p>
+          <p>Present thus at the reception, to pass over.</p>
           <img src={QR}/>
-          <p>Escuela Colombiana de Ingenieros Julio Garavito</p>
+          <p className="company">Escuela Colombiana de Ingenieros Julio Garavito</p>
           <h2>Important things you need to remember</h2>
           <p>Hour: 8:30</p>
           <p>Address: AK 45 (Autonorte) #205-59</p>
           <p>Invited: Oswaldo Castillo</p>
         </div>
       </div>
+      </>
 
-    );
-  }
+  );
+}
 }
--- src/Meeting/Assistance.js
+++ src/Meeting/Assistance.js
@@ -2,7 +2,12 @@ import React from "react";
 import './Assistance.css';
 import escuela from './../img/escuela.png';
 import Link from "@material-ui/core/Link";
-
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import KeyboardArrowLeft from "@material-ui/core/es/internal/svg-icons/KeyboardArrowLeft";
+import Typography from "@material-ui/core/Typography";
 
 export class Assistance extends React.Component {
 
@@ -12,9 +17,21 @@ export class Assistance extends React.Component {
 
   render(){
     return (
+
+      <>
+      <CssBaseline/>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className="menuButton" color="inherit" aria-label="Menu">
+            <KeyboardArrowLeft/>
+          </IconButton>
+          <Typography variant="h6" color="inherit" className="grow">
+            Confirm Assistance
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <div className="content">
         <div>
-          <p>Confirm Assistance</p>
           <p>Wednesday 26 February 2019</p>
           <img src={escuela}/>
           <p>Hour: 8:30</p>
@@ -26,7 +43,7 @@ export class Assistance extends React.Component {
           <div className="decline"> DECLINE</div>
         </div>
       </div>
-
+      </>
     );
   }
 }
