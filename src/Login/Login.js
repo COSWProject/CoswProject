3e3c198dc9b91f5e5e2b75fe376c1de6dd542e9d
danielagon <daniela.gonzalezramirez@hotmail.com>
2019-03-12 19:36:21 -0500

    Modified Login and navigability

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {Component} from 'react';
 import TextField from '@material-ui/core/TextField';
-import Paper from "@material-ui/core/es/Paper/Paper";
 import './Login.css';
-import logo from './../img/Logo.png';
-import user from './../img/user.svg';
-import password from './../img/password.svg';
-import {Button, Checkbox, FormControlLabel} from "@material-ui/core";
+import VpnKey from '@material-ui/icons/VpnKey';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import {Button, Checkbox, FormControlLabel, Typography} from "@material-ui/core";
 import Link from "@material-ui/core/Link";
+import withStyles from "@material-ui/core/styles/withStyles";
+import SimpleModal from "./../Company/SimpleModal";
 
-export class Login extends React.Component {
+const styles = theme => ({
+    text: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: "100%"
+    }, avatar: {
+        backgroundColor: "black",
+        margin: theme.spacing.unit,
+        textAlign: "center"
+    }, card: {
+        marginTop: "7%"
+    }, button: {
+        width: "70%",
+        position: "left"
+    }, divButton: {
+        textAlign: "right"
+    }
+});
+
+class Login extends Component {
 
     constructor(props) {
         super(props);
@@ -20,7 +39,6 @@ export class Login extends React.Component {
 
     handleLogin() {
         if (this.state.email === localStorage.getItem('email') && this.state.password === localStorage.getItem('password')) {
-            localStorage.setItem('page', 'home');
             console.log('Logged');
         }
         this.setState({
@@ -30,6 +48,7 @@ export class Login extends React.Component {
     }
 
     handleEmailChange(e) {
+        alert("email");
         this.setState({
             email: e.target.value
         })
@@ -42,21 +61,82 @@ export class Login extends React.Component {
     }
 
     render() {
-        return (
-            <Paper className="paper" elevation={6}>
-                <img src={logo} alt="logo"/>
-                <form className="form" onSubmit={this.handleLogin}>
-                    <TextField required label="Username" margin="normal" onChange={this.handleEmailChange} InputProps={{startAdornment: (<img src={user} alt="user"/>)}}/>
+
+        const {classes} = this.props;
+
+        const inputs = [
+            {
+                label: "Username",
+                onChange: this.handleEmailChange,
+                icon: <AccountCircle className={classes.icon}/>
+            },
+            {
+                label: "Password",
+                type: "password",
+                onChange: this.handlePasswordChange,
+                icon: <VpnKey className={classes.icon}/>
+            }
+        ];
+
+        const inputTexts = inputs.map((x) => {
+            return (
+                <>
+                    <TextField
+                        required
+                        className={classes.text}
+                        label={x.label}
+                        margin="normal"
+                        onChange={x.onchange}
+                        type={x.type}
+                        InputProps={{
+                            startAdornment: (
+                                x.icon
+                            )
+                        }}
+                    />
                     <br/>
-                    <TextField required type="password" label="Password" margin="normal" onChange={this.handlePasswordChange} InputProps={{startAdornment: (<img src={password} alt="password"/>)}}/>
-                    <br/>
-                    <FormControlLabel control={<Checkbox color="primary"/>} label="Keep me logged in"/>
-                    <br/>
-                    <Button type="submit" variant="outlined" color="primary">Login</Button>
-                </form>
+                </>
+            );
+        });
+
+        const form = (
+            <form className={classes.form}>
+                <Typography component="h1" variant="h4" color="primary">
+                    EasyAccess
+                </Typography>
                 <br/>
-                <Link href="/signup" variant="body2">Don't have an account? Create Account</Link>
-            </Paper>
+                {inputTexts}
+                <br/>
+                <FormControlLabel
+                    control={<Checkbox color="primary"/>}
+                    label="Keep me logged in"/>
+                <br/>
+                <Button
+                    type="submit"
+                    variant="outlined"
+                    color="primary"
+                    className={classes.button}
+                    action={this.handleLogin}
+                    href="/schedule"
+                >
+                    Sign in
+                </Button>
+                <br/>
+                <Link
+                    href="/signup"
+                    variant="body2"
+                >
+                    Don't have an account? Create Account
+                </Link>
+            </form>
+        );
+
+        return (
+            <>
+                <SimpleModal elements={form} buttonName="Login"/>
+            </>
         );
     }
 }
+
+export default withStyles(styles)(Login);
