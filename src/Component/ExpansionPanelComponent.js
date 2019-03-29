3859071110a596eb2a22a5e44d386ac59b707532
Camilo Lopez <camilino123@gmail.com>
2019-03-28 21:19:42 -0500

    Expansion panel component created

---
+++ src/Component/ExpansionPanelComponent.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Typography from "@material-ui/core/Typography";
+
+const styles = theme => ({
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+});
+
+class ExpansionPanelComponent extends Component {
+    render() {
+
+        const {classes} = this.props;
+
+        return (
+            <ExpansionPanel>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+                    <Typography className={classes.heading}>{this.props.title}</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    {this.props.elements}
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+        );
+    }
+}
